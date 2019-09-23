a916dfd4f7cd757d8cb5e38bf006a2ff56923efe
Joshua Spann <jspann128@gmail.com>
2019-09-22 19:41:33 -0500

    Test changes

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@ type src\wip.htm > wip.htm
 for %%i in (src\*.md) do type src\header.md > %%~ni.md done
 del *.htm
 copy src\*.htm .\
-for %%i in (src\*.md) do type src\header.md >> %%~ni.md
 for %%i in (src\*.md) do type src\%%~ni.md >> %%~ni.md
 for %%i in (*.md) do pandoc -f markdown -t html5 %%~ni.md >> %%~ni.htm && del %%~ni.md
 del header.htm
